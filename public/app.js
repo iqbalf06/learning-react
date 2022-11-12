36823af78d1cb6b83eff73464566daabbc8a5451
Iqbal Faisal <faisaelz@gmail.com>
2022-11-12 10:46:00 +0700

    to do list

--- public/app.js
+++ public/app.js
@@ -1,15 +1,35 @@
 const root = document.querySelector('#root');
 function App() {
   const [activity, setActivity] = React.useState('');
+  const [edit, setEdit] = React.useState({});
   const [todos, setTodos] = React.useState([]);
+  const [message, setMessage] = React.useState('');
   function generateId() {
     return Date.now();
   }
-  function addTodoHandler(event) {
+  function saveTodoHandler(event) {
     event.preventDefault();
+    if (!activity) {
+      return setMessage('Aktivitas tidak boleh kosong!');
+    }
+    setMessage('');
+    if (edit.id) {
+      const updatedTodo = {
+        ...edit,
+        activity
+      };
+      const editTodoIndex = todos.findIndex(function (todo) {
+        return todo.id == edit.id;
+      });
+      const updatedTodos = [...todos];
+      updatedTodos[editTodoIndex] = updatedTodo;
+      setTodos(updatedTodos);
+      return cancelEditHandler();
+    }
     setTodos([...todos, {
       id: generateId(),
-      activity: activity
+      activity,
+      done: false
     }]);
     setActivity('');
   }
@@ -18,9 +38,34 @@ function App() {
       return todo.id !== todoId;
     });
     setTodos(filteredTodos);
+    if (edit.id) cancelEditHandler();
   }
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Simple To do List"), /*#__PURE__*/React.createElement("form", {
-    onSubmit: addTodoHandler
+  function editTodoHandler(todo) {
+    setActivity(todo.activity);
+    setEdit(todo);
+  }
+  function cancelEditHandler() {
+    setEdit({});
+    setActivity('');
+  }
+  function doneTodoHandler(todo) {
+    const updatedTodo = {
+      ...todo,
+      done: todo.done ? false : true
+    };
+    const editTodoIndex = todos.findIndex(function (currentTodo) {
+      return currentTodo.id == todo.id;
+    });
+    const updatedTodos = [...todos];
+    updatedTodos[editTodoIndex] = updatedTodo;
+    setTodos(updatedTodos);
+  }
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Simple To do List"), message && /*#__PURE__*/React.createElement("div", {
+    style: {
+      color: 'red'
+    }
+  }, message), /*#__PURE__*/React.createElement("form", {
+    onSubmit: saveTodoHandler
   }, /*#__PURE__*/React.createElement("input", {
     type: "text",
     placeholder: "Nama aktivitas",
@@ -30,12 +75,20 @@ function App() {
     }
   }), /*#__PURE__*/React.createElement("button", {
     type: "submit"
-  }, "Tambah")), /*#__PURE__*/React.createElement("ul", null, todos.map(function (todo) {
+  }, edit.id ? 'Simpan' : 'Tambah', " "), edit.id && /*#__PURE__*/React.createElement("button", {
+    onClick: cancelEditHandler
+  }, "Batal Edit")), todos.length > 0 ? /*#__PURE__*/React.createElement("ul", null, todos.map(function (todo) {
     return /*#__PURE__*/React.createElement("li", {
       key: todo.id
-    }, todo.activity, /*#__PURE__*/React.createElement("button", {
+    }, /*#__PURE__*/React.createElement("input", {
+      type: "checkbox",
+      checked: todo.done,
+      onChange: doneTodoHandler.bind(this, todo)
+    }), todo.activity, "(", todo.done ? 'Selesai' : 'Belum Selesai', ")", /*#__PURE__*/React.createElement("button", {
+      onClick: editTodoHandler.bind(this, todo)
+    }, "Edit"), /*#__PURE__*/React.createElement("button", {
       onClick: removeTodoHandler.bind(this, todo.id)
     }, "Hapus"));
-  })));
+  })) : /*#__PURE__*/React.createElement("i", null, "Tidak ada todo"));
 }
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- src/app.js
+++ src/app.js
@@ -2,20 +2,47 @@ const root = document.querySelector('#root')
 
 function App() {
     const [activity, setActivity] = React.useState('');
-    const [todos, setTodos] = React.useState([])
+    const [edit, setEdit] = React.useState({});
+    const [todos, setTodos] = React.useState([]);
+    const [message, setMessage] = React.useState('');
 
-    function generateId() {
+    function generateId()  {
         return Date.now();
     }
      
-    function addTodoHandler(event) {
+    function saveTodoHandler(event) {
         event.preventDefault();
+
+        if(!activity) {
+            return setMessage('Aktivitas tidak boleh kosong!')
+        }
+
+        setMessage('')
+
+        if(edit.id) {
+            const updatedTodo = {
+                ...edit,
+                activity,
+            };
+
+            const editTodoIndex = todos.findIndex(function(todo) {
+                return todo.id == edit.id;
+            });
+
+            const updatedTodos = [...todos];
+            updatedTodos[editTodoIndex] = updatedTodo;
+
+            setTodos(updatedTodos);
+
+            return cancelEditHandler();
+        }
 
         setTodos([
             ...todos, 
             {
                 id: generateId(),
-                activity: activity,
+                activity,
+                done: false
             },
         ]);
         setActivity('');    
@@ -27,12 +54,39 @@ function App() {
         });
 
         setTodos(filteredTodos)
+        if(edit.id) cancelEditHandler();
+    }
+
+    function editTodoHandler(todo) {
+        setActivity(todo.activity)
+        setEdit(todo);
+    }
+
+    function cancelEditHandler() {
+        setEdit({});
+        setActivity('');
+    }
+
+    function doneTodoHandler(todo) {
+        const updatedTodo = {
+            ...todo,
+            done: todo.done ? false : true,
+        };
+        const editTodoIndex = todos.findIndex(function(currentTodo) {
+            return currentTodo.id == todo.id
+        });
+
+        const updatedTodos = [...todos];
+        updatedTodos[editTodoIndex] = updatedTodo;
+
+        setTodos(updatedTodos);
     }
 
     return (
         <>
         <h1>Simple To do List</h1>
-        <form onSubmit={addTodoHandler}>
+        {message && <div style={{color: 'red'}}>{message}</div>}
+        <form onSubmit={saveTodoHandler}>
             <input type="text" 
             placeholder="Nama aktivitas" 
             value={activity}
@@ -40,19 +94,27 @@ function App() {
                 setActivity(event.target.value);
             }}>
             </input>
-            <button type="submit">Tambah</button>
+            <button type="submit">{edit.id ? 'Simpan' : 'Tambah'} </button>
+            {edit.id && <button onClick={cancelEditHandler}>Batal Edit</button>}
         </form>
+        {todos.length > 0 ? (
         <ul>
             {todos.map(function (todo) {
                 return (
-                <li key={todo.id}>{todo.activity}
-                <button onClick={removeTodoHandler.bind(this, todo.id)}>Hapus</button>
+                <li key={todo.id}>
+                    <input type="checkbox" checked={todo.done} onChange={doneTodoHandler.bind(this, todo)} /> 
+                    {todo.activity}({todo.done ? 'Selesai' : 'Belum Selesai'})
+                <button onClick={editTodoHandler.bind(this, todo)}>Edit</button>
+                <button onClick={removeTodoHandler.bind(this, todo.id)}>Hapus</button>  
                 </li>
                 );
             })}
         </ul>
+       ) : (
+        <i>Tidak ada todo</i>
+        )}
         </>
-    )
+    );
 }
 
 ReactDOM.render(<App />, root);
